a0199ff019b8f2112233e53d6c8aae3cd61f23b3
Arjunkadakia <kadakia.arjun@gmail.com>
2025-09-23 18:02:29 +0530

    Add files via upload

---
+++ fifthCode.py
@@ -0,0 +1,107 @@
+num1=int(input("enter number"))
+num2=int(input("enter number"))
+num3=int(input("enter number"))
+num4=int(input("enter number"))
+num5=int(input("enter number"))
+num6=int(input("enter number"))
+num7=int(input("enter number"))
+num8=int(input("enter number"))
+num9=int(input("enter number"))
+num10=int(input("enter number"))
+num11=int(input("enter number"))
+num12=int(input("enter number"))
+num13=int(input("enter number"))
+num14=int(input("enter number"))
+num15=int(input("enter number"))
+num16=int(input("enter number"))
+num17=int(input("enter number"))
+num18=int(input("enter number"))
+num19=int(input("enter number"))
+num20=int(input("enter number"))
+print("the numbers are",num1,num2,num3,num4,num5,num6,num7,num8,num9,num10,num11,num12,num13,num14,num15,num16,num17,num18,num19,num20)
+if num1>num2 and num1>num3 and num1>num4 and num1>num5 and num1>num6 and num1>num7 and num1>num8 and num1>num9 and num1>num10 and num1>num11 and num1>num12 and num1>num13 and num1>num14 and num1>num15 and num1>num16 and num1>num17 and num1>num18 and num1>num19 and num1>num20:
+    print(num1,"is the greatest")
+elif num2>num3 and num2>num4 and num2>num5 and num2>num6 and num2>num7 and num2>num8 and num2>num9 and num2>num10 and num2>num11 and num2>num12 and num2>num13 and num2>num14 and num2>num15 and num2>num16 and num2>num17 and num2>num18 and num2>num19 and num2>num20:
+    print(num2,"is the greatest")
+elif num3>num4 and num3>num5 and num3>num6 and num3>num7 and num3>num8 and num3>num9 and num3>num10 and num3>num11 and num3>num12 and num3>num13 and num3>num14 and num3>num15 and num3>num16 and num3>num17 and num3>num18 and num3>num19 and num3>num20:
+    print(num3,"is the greatest")
+elif num4>num5 and num4>num6 and num4>num7 and num4>num8 and num4>num9 and num4>num10 and num4>num11 and num4>num12 and num4>num13 and num4>num14 and num4>num15 and num4>num16 and num4>num17 and num4>num18 and num4>num19 and num4>num20:
+    print(num4,"is the greatest")
+elif num5>num6 and num5>num7 and num5>num8 and num5>num9 and num5>num10 and num5>num11 and num5>num12 and num5>num13 and num5>num14 and num5>num15 and num5>num16 and num5>num17 and num5>num18 and num5>num19 and num5>num20:
+    print(num5,"is the greatest")
+elif num6>num7 and num6>num8 and num6>num9 and num6>num10 and num6>num11 and num6>num12 and num6>num13 and num6>num14 and num6>num15 and num6>num16 and num6>num17 and num6>num18 and num6>num19 and num6>num20:
+    print(num6,"is the greatest")
+elif num7>num8 and num7>num9 and num7>num10 and num7>num11 and num7>num12 and num7>num13 and num7>num14 and num7>num15 and num7>num16 and num7>num17 and num7>num18 and num7>num19 and num7>num20:
+    print(num7,"is the greatest")
+elif num8>num9 and num8>num10 and num8>num11 and num8>num12 and num8>num13 and num8>num14 and num8>num15 and num8>num16 and num8>num17 and num8>num18 and num8>num19 and num8>num20:
+    print(num8,"is the greatest")
+elif num9>num10 and num9>num11 and num9>num12 and num9>num13 and num9>num14 and num9>num15 and num9>num16 and num9>num17 and num9>num18 and num9>num19 and num9>num20:
+    print(num9,"is the greatest")
+elif num10>num11 and num10>num12 and num10>num13 and num10>num14 and num10>num15 and num10>num16 and num10>num17 and num10>num18 and num10>num19 and num10>num20:
+    print(num10,"is the greatest")
+elif num11>num12 and num11>num13 and num11>num14 and num11>num15 and num11>num16 and num11>num17 and num11>num18 and num11>num19 and num11>num20:
+    print(num11<"is the greatest")
+elif num12>num13 and num12>num14 and num12>num15 and num12>num16 and num12>num17 and num12>num18 and num12>num19 and num12>num20:
+    print(num12,"is the greatest")
+elif num13>num14 and num13>num15 and num13>num16 and num13>num17 and num13>num18 and num13>num19 and num13>num20:
+    print(num13,"is the greatest")
+elif num14>num15 and num14>num16 and num14>num17 and num14>num18 and num14>num19 and num14>num20:
+    print(num14,"is the greatest")
+elif num15>num16 and num15>num17 and num15>num18 and num15>num19 and num15>num20:
+    print(num15,"is the greatest")
+elif num16>num17 and num16>num18 and num16>num19 and num16>num20:
+    print(num16,"is the greatest")
+elif num17>num18 and num17>num19 and num17>num20:
+    print(num17,"is the greatest")
+elif num18>num19 and num18>num20:
+    print(num18,"is the greatest")
+elif num19>num20:
+    print(num19,"is the greatest")
+else:
+    print(num20,"is the greatest")
+if num1<num2 and num1<num3 and num1<num4 and num1<num5 and num1<num6 and num1<num7 and num1<num8 and num1<num9 and num1<num10 and num1<num11 and num1<num12 and num1<num13 and num1<num14 and num1<num15 and num1<num16 and num1<num17 and num1<num18 and num1<num19 and num1<num20:
+    print(num1,"is the least")
+elif num2<num3 and num2<num4 and num2<num5 and num2<num6 and num2<num7 and num2<num8 and num2<num9 and num2<num10 and num2<num11 and num2<num12 and num2<num13 and num2<num14 and num2<num15 and num2<num16 and num2<num17 and num2<num18 and num2<num19 and num2<num20:
+    print(num2,"is the least")
+elif num3<num4 and num3<num5 and num3<num6 and num3<num7 and num3<num8 and num3<num9 and num3<num10 and num3<num11 and num3<num12 and num3<num13 and num3<num14 and num3<num15 and num3<num16 and num3<num17 and num3<num18 and num3<num19 and num3<num20:
+    print(num3,"is the least")
+elif num4<num5 and num4<num6 and num4<num7 and num4<num8 and num4<num9 and num4<num10 and num4<num11 and num4<num12 and num4<num13 and num4<num14 and num4<num15 and num4<num16 and num4<num17 and num4<num18 and num4<num19 and num4<num20:
+    print(num4,"is the least")
+elif num5<num6 and num5<num7 and num5<num8 and num5<num9 and num5<num10 and num5<num11 and num5<num12 and num5<num13 and num5<num14 and num5<num15 and num5<num16 and num5<num17 and num5<num18 and num5<num19 and num5<num20:
+    print(num5,"is the least ")
+elif num6<num7 and num6<num8 and num6<num9 and num6<num10 and num6<num11 and num6<num12 and num6<num13 and num6<num14 and num6<num15 and num6<num16 and num6<num17 and num6<num18 and num6<num19 and num6<num20:
+    print(num6,"is the least")
+elif num7<num8 and num7<num9 and num7<num10 and num7<num11 and num7<num12 and num7<num13 and num7<num14 and num7<num15 and num7<num16 and num7<num17 and num7<num18 and num7<num19 and num7<num20:
+    print(num7,"is the least")
+elif num8<num9 and num8<num10 and num8<num11 and num8<num12 and num8<num13 and num8<num14 and num8<num15 and num8<num16 and num8<num17 and num8<num18 and num8<num19 and num8<num20:
+    print(num8,"is the least")
+elif num9<num10 and num9<num11 and num9<num12 and num9<num13 and num9<num14 and num9<num15 and num9<num16 and num9<num17 and num9<num18 and num9<num19 and num9<num20:
+    print(num9,"is the least")
+elif num10<num11 and num10<num12 and num10<num13 and num10<num14 and num10<num15 and num10<num16 and num10<num17 and num10<num18 and num10<num19 and num10<num20:
+    print(num10,"is the least")
+elif num11<num12 and num11<num13 and num11<num14 and num11<num15 and num11<num16 and num11<num17 and num11<num18 and num11<num19 and num11<num20:
+    print(num11<"is the least")
+elif num12<num13 and num12<num14 and num12<num15 and num12<num16 and num12<num17 and num12<num18 and num12<num19 and num12<num20:
+    print(num12,"is the least")
+elif num13<num14 and num13<num15 and num13<num16 and num13<num17 and num13<num18 and num13<num19 and num13<num20:
+    print(num13,"is the least")
+elif num14<num15 and num14<num16 and num14<num17 and num14<num18 and num14<num19 and num14<num20:
+    print(num14,"is the least")
+elif num15<num16 and num15<num17 and num15<num18 and num15<num19 and num15<num20:
+    print(num15,"is the least")
+elif num16<num17 and num16<num18 and num16<num19 and num16<num20:
+    print(num16,"is the least")
+elif num17<num18 and num17<num19 and num17<num20:
+    print(num17,"is the least")
+elif num18<num19 and num18<num20:
+    print(num18,"is the least")
+elif num19<num20:
+    print(num19,"is the least")
+else:
+    print(num20,"is the least")
+sum=num1+num2+num3+num4+num5+num6+num7+num8+num9+num10+num11+num12+num13+num14+num15+num16+num17+num18+num19+num20
+print(sum)
+if sum%2==0:
+    print("the average is",sum/2)
+else:
+    print("the average is",(sum/2)+0.5)
