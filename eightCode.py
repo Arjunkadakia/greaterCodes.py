be440c74d1bd1b6ee65050f42caf4ac20f38e5b9
Arjun Kadakia <kadakia.arjun@gmail.com>
2025-10-31 21:30:32 +0530

    Add files via upload

---
+++ eightCode.py
@@ -0,0 +1,157 @@
+num1=int(input("enter number"))
+num2=int(input("enter number"))
+num3=int(input("enter number"))
+num4=int(input("enter number"))
+num5=int(input("enter number"))
+num6=int(input("enter number"))
+num7=int(input("enter number"))
+num8=int(input("enter number"))
+num9=int(input("enter number"))
+num10=int(input("enter number"))
+num11=int(input("enter number"))
+num12=int(input("enter number"))
+num13=int(input("enter number"))
+num14=int(input("enter number"))
+num15=int(input("enter number"))
+num16=int(input("enter number"))
+num17=int(input("enter number"))
+num18=int(input("enter number"))
+num19=int(input("enter number"))
+num20=int(input("enter number"))
+num21=int(input("enter number"))
+num22=int(input("enter number"))
+num23=int(input("enter number"))
+num24=int(input("enter number"))
+num25=int(input("enter number"))
+num26=int(input("enter number"))
+num27=int(input("enter number"))
+num28=int(input("enter number"))
+num29=int(input("enter number"))
+num30=int(input("enter number"))
+print("the numbers are",num1,num2,num3,num4,num5,num6,num7,num8,num9,num10,num11,num12,num13,num14,num15,num16,num17,num18,num19,num20,num21,num22,num23,num24,num25,num26,num27,num28,num29,num30)
+if num1>num2 and num1>num3 and num1>num4 and num1>num5 and num1>num6 and num1>num7 and num1>num8 and num1>num9 and num1>num10 and num1>num11 and num1>num12 and num1>num13 and num1>num14 and num1>num15 and num1>num16 and num1>num17 and num1>num18 and num1>num19 and num1>num20 and num1>num21 and num1>num22 and num1>num23 and num1>num24 and num1>num25 and num1>num26 and num1>num27 and num1>num28 and num1>num29 and num1>num30:
+    print(num1,"is the greatest")
+elif num2>num3 and num2>num4 and num2>num5 and num2>num6 and num2>num7 and num2>num8 and num2>num9 and num2>num10 and num2>num11 and num2>num12 and num2>num13 and num2>num14 and num2>num15 and num2>num16 and num2>num17 and num2>num18 and num2>num19 and num2>num20 and num2>num21 and num2>num22 and num2>num23 and num2>num24 and num2>num25 and num2>num26 and num2>num27 and num2>num28 and num2>num29 and num2>num30:
+    print(num2,"is the greatest")
+elif num3>num4 and num3>num5 and num3>num6 and num3>num7 and num3>num8 and num3>num9 and num3>num10 and num3>num11 and num3>num12 and num3>num13 and num3>num14 and num3>num15 and num3>num16 and num3>num17 and num3>num18 and num3>num19 and num3>num20 and num3>num21 and num3>num22 and num3>num23 and num3>num24 and num3>num25 and num3>num26 and num3>num27 and num3>num28 and num3>num29 and num3>num30:
+    print(num3,"is the greatest")
+elif num4>num5 and num4>num6 and num4>num7 and num4>num8 and num4>num9 and num4>num10 and num4>num11 and num4>num12 and num4>num13 and num4>num14 and num4>num15 and num4>num16 and num4>num17 and num4>num18 and num4>num19 and num4>num20 and num4>num21 and num4>num22 and num4>num23 and num4>num24 and num4>num25 and num4>num26 and num4>num27 and num4>num28 and num4>num29 and num4>num30:
+    print(num4,"is the greatest")
+elif num5>num6 and num5>num7 and num5>num8 and num5>num9 and num5>num10 and num5>num11 and num5>num12 and num5>num13 and num5>num14 and num5>num15 and num5>num16 and num5>num17 and num5>num18 and num5>num19 and num5>num20 and num5>num21 and num5>num22 and num5>num23 and num5>num24 and num5>num25 and num5>num26 and num5>num27 and num5>num28 and num5>num29 and num5>num30:
+    print(num5,"is the greatest")
+elif num6>num7 and num6>num8 and num6>num9 and num6>num10 and num6>num11 and num6>num12 and num6>num13 and num6>num14 and num6>num15 and num6>num16 and num6>num17 and num6>num18 and num6>num19 and num6>num20 and num6>num21 and num6>num22 and num6>num23 and num6>num24 and num6>num25 and num6>num26 and num6>num27 and num6>num28 and num6>num29 and num6>num30:
+    print(num6,"is the greatest")
+elif num7>num8 and num7>num9 and num7>num10 and num7>num11 and num7>num12 and num7>num13 and num7>num14 and num7>num15 and num7>num16 and num7>num17 and num7>num18 and num7>num19 and num7>num20 and num7>num21 and num7>num22 and num7>num23 and num7>num24 and num7>num25 and num7>num26 and num7>num27 and num7>num28 and num7>num29 and num7>num30:
+    print(num7,"is the greatest")
+elif num8>num9 and num8>num10 and num8>num11 and num8>num12 and num8>num13 and num8>num14 and num8>num15 and num8>num16 and num8>num17 and num8>num18 and num8>num19 and num8>num20 and num8>num21 and num8>num22 and num8>num23 and num8>num24 and num8>num25 and num8>num26 and num8>num27 and num8>num28 and num8>num29 and num8>num30:
+    print(num8,"is the greatest")
+elif num9>num10 and num9>num11 and num9>num12 and num9>num13 and num9>num14 and num9>num15 and num9>num16 and num9>num17 and num9>num18 and num9>num19 and num9>num20 and num9>num21 and num9>num22 and num9>num23 and num9>num24 and num9>num25 and num9>num26 and num9>num27 and num9>num28 and num9>num29 and num9>num30:
+    print(num9,"is the greatest")
+elif num10>num11 and num10>num12 and num10>num13 and num10>num14 and num10>num15 and num10>num16 and num10>num17 and num10>num18 and num10>num19 and num10>num20 and num10>num21 and num10>num22 and num10>num23 and num10>num24 and num10>num25 and num10>num26 and num10>num27 and num10>num28 and num10>num29 and num10>num30:
+    print(num10,"is the greatest")
+elif num11>num12 and num11>num13 and num11>num14 and num11>num15 and num11>num16 and num11>num17 and num11>num18 and num11>num19 and num11>num20 and num11>num21 and num11>num22 and num11>num23 and num11>num24 and num11>num25 and num11>num26 and num11>num27 and num11>num28 and num11>num29 and num11>num30:
+    print(num11<"is the greatest")
+elif num12>num13 and num12>num14 and num12>num15 and num12>num16 and num12>num17 and num12>num18 and num12>num19 and num12>num20 and num12>num21 and num12>num22 and num12>num23 and num12>num24 and num12>num25 and num12>num26 and num12>num27 and num12>num28 and num12>num29 and num12>num30:
+    print(num12,"is the greatest")
+elif num13>num14 and num13>num15 and num13>num16 and num13>num17 and num13>num18 and num13>num19 and num13>num20 and num13>num21 and num13>num22 and num13>num23 and num13>num24 and num13>num25 and num13>num26 and num13>num27 and num13>num28 and num13>num29 and num13>num30:
+    print(num13,"is the greatest")
+elif num14>num15 and num14>num16 and num14>num17 and num14>num18 and num14>num19 and num14>num20 and num14>num21 and num14>num22 and num14>num23 and num14>num24 and num14>num25 and num14>num26 and num14>num27 and num14>num28 and num14>num29 and num14>num30:
+    print(num14,"is the greatest")
+elif num15>num16 and num15>num17 and num15>num18 and num15>num19 and num15>num20 and num15>num21 and num15>num22 and num15>num23 and num15>num24 and num15>num25 and num15>num26 and num15>num27 and num15>num28 and num15>num29 and num15>num30:
+    print(num15,"is the greatest")
+elif num16>num17 and num16>num18 and num16>num19 and num16>num20 and num16>num21 and num16>num22 and num16>num23 and num16>num24 and num16>num25 and num16>num26 and num16>num27 and num16>num28 and num16>num29 and num16>num30:
+    print(num16,"is the greatest")
+elif num17>num18 and num17>num19 and num17>num20 and num17>num21 and num17>num22 and num17>num23 and num17>num24 and num17>num25 and num17>num26 and num17>num27 and num17>num28 and num17>num29 and num17>num30:
+    print(num17,"is the greatest")
+elif num18>num19 and num18>num20 and num18>num21 and num18>num22 and num18>num23 and num18>num24 and num18>num25 and num18>num26 and num18>num27 and num18>num28 and num18>num29 and num18>num30:
+    print(num18,"is the greatest")
+elif num19>num20 and num19>num21 and num19>num22 and num19>num23 and num19>num24 and num19>num25 and num19>num26 and num19>num27 and num19>num28 and num19>num29 and num19>num30:
+    print(num19,"is the greatest")
+elif num20>num21 and num20>num22 and num20>num23 and num20>num24 and num20>num25 and num20>num26 and num20>num27 and num20>num28 and num20>num29 and num20>num30:
+    print(num20,"is the greatest")
+elif num21>num22 and num21>num23 and num21>num24 and num21>num25 and num21>num26 and num21>num27 and num21>num28 and num21>num29 and num21>num30:
+    print(num21,"is the greatest")
+elif num22>num23 and num22>num24 and num22>num25 and num22>num26 and num22>num27 and num22>num28 and num22>num29 and num22>num30:
+    print(num22,"is the greatest")
+elif num23>num24 and num23>num25 and num23>num26 and num23>num27 and num23>num28 and num23>num29 and num23>num30:
+    print(num23,"is the greatest")
+elif num24>num25 and num24>num26 and num24>num27 and num24>num28 and num24>num29 and num24>num30:
+    print(num24,"is the greatest")
+elif num25>num26 and num25>num27 and num25>num28 and num25>num29 and num25>num30:
+    print(num25,"is the greatest")
+elif num26>num27 and num26>num28 and num26>num29 and num26>num30:
+    print(num26,"is the greatest")
+elif num27>num28 and num27>num29 and num27>num30:
+    print(num27,"is the greatest")
+elif num28>num29 and num28>num30:
+    print(num28,"is the greatest")
+elif num29>num30:
+    print(num29,"is the greatest")
+else:
+    print(num30,"is the greatest")
+if num1<num2 and num1<num3 and num1<num4 and num1<num5 and num1<num6 and num1<num7 and num1<num8 and num1<num9 and num1<num10 and num1<num11 and num1<num12 and num1<num13 and num1<num14 and num1<num15 and num1<num16 and num1<num17 and num1<num18 and num1<num19 and num1<num20 and num1<num21 and num1<num22 and num1<num23 and num1<num24 and num1<num25 and num1<num26 and num1<num27 and num1<num28 and num1<num29 and num1<num30:
+    print(num1,"is the least")
+elif num2<num3 and num2<num4 and num2<num5 and num2<num6 and num2<num7 and num2<num8 and num2<num9 and num2<num10 and num2<num11 and num2<num12 and num2<num13 and num2<num14 and num2<num15 and num2<num16 and num2<num17 and num2<num18 and num2<num19 and num2<num20 and num2<num21 and num2<num22 and num2<num23 and num2<num24 and num2<num25 and num2<num26 and num2<num27 and num2<num28 and num2<num29 and num2<num30:
+    print(num2,"is the least")
+elif num3<num4 and num3<num5 and num3<num6 and num3<num7 and num3<num8 and num3<num9 and num3<num10 and num3<num11 and num3<num12 and num3<num13 and num3<num14 and num3<num15 and num3<num16 and num3<num17 and num3<num18 and num3<num19 and num3<num20 and num3<num21 and num3<num22 and num3<num23 and num3<num24 and num3<num25 and num3<num26 and num3<num27 and num3<num28 and num3<num29 and num3<num30:
+    print(num3,"is the least")
+elif num4<num5 and num4<num6 and num4<num7 and num4<num8 and num4<num9 and num4<num10 and num4<num11 and num4<num12 and num4<num13 and num4<num14 and num4<num15 and num4<num16 and num4<num17 and num4<num18 and num4<num19 and num4<num20 and num4<num21 and num4<num22 and num4<num23 and num4<num24 and num4<num25 and num4<num26 and num4<num27 and num4<num28 and num4<num29 and num4<num30:
+    print(num4,"is the least")
+elif num5<num6 and num5<num7 and num5<num8 and num5<num9 and num5<num10 and num5<num11 and num5<num12 and num5<num13 and num5<num14 and num5<num15 and num5<num16 and num5<num17 and num5<num18 and num5<num19 and num5<num20 and num5<num21 and num5<num22 and num5<num23 and num5<num24 and num5<num25 and num5<num26 and num5<num27 and num5<num28 and num5<num29 and num5<num30:
+    print(num5,"is the least")
+elif num6<num7 and num6<num8 and num6<num9 and num6<num10 and num6<num11 and num6<num12 and num6<num13 and num6<num14 and num6<num15 and num6<num16 and num6<num17 and num6<num18 and num6<num19 and num6<num20 and num6<num21 and num6<num22 and num6<num23 and num6<num24 and num6<num25 and num6<num26 and num6<num27 and num6<num28 and num6<num29 and num6<num30:
+    print(num6,"is the least")
+elif num7<num8 and num7<num9 and num7<num10 and num7<num11 and num7<num12 and num7<num13 and num7<num14 and num7<num15 and num7<num16 and num7<num17 and num7<num18 and num7<num19 and num7<num20 and num7<num21 and num7<num22 and num7<num23 and num7<num24 and num7<num25 and num7<num26 and num7<num27 and num7<num28 and num7<num29 and num7<num30:
+    print(num7,"is the least")
+elif num8<num9 and num8<num10 and num8<num11 and num8<num12 and num8<num13 and num8<num14 and num8<num15 and num8<num16 and num8<num17 and num8<num18 and num8<num19 and num8<num20 and num8<num21 and num8<num22 and num8<num23 and num8<num24 and num8<num25 and num8<num26 and num8<num27 and num8<num28 and num8<num29 and num8<num30:
+    print(num8,"is the least")
+elif num9<num10 and num9<num11 and num9<num12 and num9<num13 and num9<num14 and num9<num15 and num9<num16 and num9<num17 and num9<num18 and num9<num19 and num9<num20 and num9<num21 and num9<num22 and num9<num23 and num9<num24 and num9<num25 and num9<num26 and num9<num27 and num9<num28 and num9<num29 and num9<num30:
+    print(num9,"is the least")
+elif num10<num11 and num10<num12 and num10<num13 and num10<num14 and num10<num15 and num10<num16 and num10<num17 and num10<num18 and num10<num19 and num10<num20 and num10<num21 and num10<num22 and num10<num23 and num10<num24 and num10<num25 and num10<num26 and num10<num27 and num10<num28 and num10<num29 and num10<num30:
+    print(num10,"is the least")
+elif num11<num12 and num11<num13 and num11<num14 and num11<num15 and num11<num16 and num11<num17 and num11<num18 and num11<num19 and num11<num20 and num11<num21 and num11<num22 and num11<num23 and num11<num24 and num11<num25 and num11<num26 and num11<num27 and num11<num28 and num11<num29 and num11<num30:
+    print(num11<"is the least")
+elif num12<num13 and num12<num14 and num12<num15 and num12<num16 and num12<num17 and num12<num18 and num12<num19 and num12<num20 and num12<num21 and num12<num22 and num12<num23 and num12<num24 and num12<num25 and num12<num26 and num12<num27 and num12<num28 and num12<num29 and num12<num30:
+    print(num12,"is the least")
+elif num13<num14 and num13<num15 and num13<num16 and num13<num17 and num13<num18 and num13<num19 and num13<num20 and num13<num21 and num13<num22 and num13<num23 and num13<num24 and num13<num25 and num13<num26 and num13<num27 and num13<num28 and num13<num29 and num13<num30:
+    print(num13,"is the least")
+elif num14<num15 and num14<num16 and num14<num17 and num14<num18 and num14<num19 and num14<num20 and num14<num21 and num14<num22 and num14<num23 and num14<num24 and num14<num25 and num14<num26 and num14<num27 and num14<num28 and num14<num29 and num14<num30:
+    print(num14,"is the least")
+elif num15<num16 and num15<num17 and num15<num18 and num15<num19 and num15<num20 and num15<num21 and num15<num22 and num15<num23 and num15<num24 and num15<num25 and num15<num26 and num15<num27 and num15<num28 and num15<num29 and num15<num30:
+    print(num15,"is the least")
+elif num16<num17 and num16<num18 and num16<num19 and num16<num20 and num16<num21 and num16<num22 and num16<num23 and num16<num24 and num16<num25 and num16<num26 and num16<num27 and num16<num28 and num16<num29 and num16<num30:
+    print(num16,"is the least")
+elif num17<num18 and num17<num19 and num17<num20 and num17<num21 and num17<num22 and num17<num23 and num17<num24 and num17<num25 and num17<num26 and num17<num27 and num17<num28 and num17<num29 and num17<num30:
+    print(num17,"is the least")
+elif num18<num19 and num18<num20 and num18<num21 and num18<num22 and num18<num23 and num18<num24 and num18<num25 and num18<num26 and num18<num27 and num18<num28 and num18<num29 and num18<num30:
+    print(num18,"is the least")
+elif num19<num20 and num19<num21 and num19<num22 and num19<num23 and num19<num24 and num19<num25 and num19<num26 and num19<num27 and num19<num28 and num19<num29 and num19<num30:
+    print(num19,"is the least")
+elif num20<num21 and num20<num22 and num20<num23 and num20<num24 and num20<num25 and num20<num26 and num20<num27 and num20<num28 and num20<num29 and num20<num30:
+    print(num20,"is the least")
+elif num21<num22 and num21<num23 and num21<num24 and num21<num25 and num21<num26 and num21<num27 and num21<num28 and num21<num29 and num21<num30:
+    print(num21,"is the least")
+elif num22<num23 and num22<num24 and num22<num25 and num22<num26 and num22<num27 and num22<num28 and num22<num29 and num22<num30:
+    print(num22,"is the least")
+elif num23<num24 and num23<num25 and num23<num26 and num23<num27 and num23<num28 and num23<num29 and num23<num30:
+    print(num23,"is the least")
+elif num24<num25 and num24<num26 and num24<num27 and num24<num28 and num24<num29 and num24<num30:
+    print(num24,"is the least")
+elif num25<num26 and num25<num27 and num25<num28 and num25<num29 and num25<num30:
+    print(num25,"is the least")
+elif num26<num27 and num26<num28 and num26<num29 and num26<num30:
+    print(num26,"is the least")
+elif num27<num28 and num27<num29 and num27<num30:
+    print(num27,"is the least")
+elif num28<num29 and num28<num30:
+    print(num28,"is the least")
+elif num29<num30:
+    print(num29,"is the least")
+else:
+    print(num30,"is the least")
+sum=num1+num2+num3+num4+num5+num6+num7+num8+num9+num10+num11+num12+num13+num14+num15+num16+num17+num18+num19+num20+num21+num22+num23+num24+num25+num26+num27+num28+num29+num30
+print("the sum of all digits is",sum)
+if sum%2==0:
+    print("the average is",sum/2)
+else:
+    print("the average is",(sum/2))
